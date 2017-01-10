db7e5fa85e8a025f925b63a03e7ef5e8306d9927
jpizagno <jpizagno@gmail.com>
2017-01-10 19:30:46 +0100

    changed nav to dropdown

--- inc/header.php
+++ inc/header.php
@@ -22,25 +22,15 @@
       <!--   Navigation Bar --> 
     <nav class="navbar navbar-default">
           <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Navigation  </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Welcome Login<span class="sr-only">(current)</span></a></li>
-                <li><a href="newbooking.php">Enter Bookings</a></li>
-                <li><a href="report.php">Monthly Report</a></li>
-
-              </ul>
-            </div><!-- /.navbar-collapse -->
+             <div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Navigation
+                <span class="caret"></span></button>
+                <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="index.php">Welcome Login</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="newbooking.php">Enter Bookings</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="report.php">Monthly Report</a></li>
+                </ul>
+              </div>
           </div><!-- /.container-fluid -->
     </nav>
       
