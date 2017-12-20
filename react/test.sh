4f28e86435229317058f473fe901cda4386f4ab2
jpizagno <jpizagno@gmail.com>
2017-12-20 13:22:40 +0100

    updated test ... started NPM tests

--- react/test.sh
+++ react/test.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
 set -e
-
 set -x
 
+
+echo "    "
+echo " running Backend Java and Spring Controller Tests....."
+echo "    "
+
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test -Pintegration-test -Dmaven.test.failure.ignore=false
+
+echo "    "
+echo "java Jacoco.exec test results can be found under ./target/"
+echo "    "
+
+echo "    "
+echo " running NPM ReactJS tests"
+echo "    "
+
+# npm install
+sudo yum install nodejs npm --enablerepo=epel -y
+sudo npm install n -g
+sudo n stable
+sudo npm install react-responsive-modal --save 
+sudo npm install -g create-react-app
+
+sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
+sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
+sudo npm i --save-dev react-test-renderer@16
+
+# create test project
+sudo mkdir my-app-test
+sudo chmod a+xrw my-app-test
+create-react-app my-app-test
+
+# copy software into my-app-test
+rm -rf my-app-test/src/*
+cp -R * my-app-test/
+
+# run tests
+cd my-app-test/
+npm install
+npm test
