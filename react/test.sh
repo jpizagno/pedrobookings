8618c82bbfb697a6106aef15c56a1206a3c3534b
EC2 Default User <ec2-user@ip-172-31-22-150.eu-central-1.compute.internal>
2017-12-22 14:13:19 +0000

    removed comments@

--- react/test.sh
+++ react/test.sh
@@ -25,10 +25,6 @@ sudo n stable
 sudo npm install react-responsive-modal --save 
 sudo npm install -g create-react-app
 
-# sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
-# sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
-# sudo npm i --save-dev react-test-renderer@16
-
 # create test project
 sudo mkdir my-app-test
 sudo chmod a+xrw my-app-test
@@ -36,7 +32,6 @@ create-react-app my-app-test
 
 # copy software into my-app-test
 rm -rf my-app-test/src/*
-# cp -R * my-app-test/
 rsync -av ./src/ ./my-app-test/src/ 
 
 # run tests
