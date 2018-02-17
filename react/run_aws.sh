4ae88a78ffb27ff6afcb280a2ce9a32620e3d111
EC2 Default User <ec2-user@ip-172-31-28-70.eu-central-1.compute.internal>
2018-02-17 18:57:51 +0000

    fixed localhost bug in app.js.  docker_build and run_aws will ask for public IPv4 address of MySQL database

--- react/run_aws.sh
+++ react/run_aws.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+set -x
 
 read -p 'AWS Public DNS (IPv4) from EC2 Dashboard: ' ip
 echo
 
 sed -i "s/localhost/$ip/" src/main/js/app.js
 
+rm -rf ./target/
 
-docker build -t  jpizagno/bookingwebapp .
+mvn spring-boot:run
