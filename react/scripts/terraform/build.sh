74f8999541bcf0e68a4b8a62ca80720b8c03ce95
jpizagno <jpizagno@gmail.com>
2018-12-29 16:30:15 +0100

    user_ip_address is now input variable in build.sh, so I do not always forget it

--- react/scripts/terraform/build.sh
+++ react/scripts/terraform/build.sh
@@ -4,6 +4,8 @@ set -e
 
 set -x
 
+read -p 'enter your ipv4 address: ' ipaddress
+echo
 read -p 'DB snapshot name (bookings03april2018): ' db_snapshot_name
 echo
 
@@ -11,10 +13,10 @@ export TF_LOG=INFO
 export TF_LOG_PATH=./terraform.log
 
 terraform init ec2_database/
-terraform apply -var 'db_snapshot_name='$db_snapshot_name'' -var-file="./ec2_database/terraform.tfvars" ec2_database/
+terraform apply -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress''  -var-file="./ec2_database/terraform.tfvars" ec2_database/
 
 terraform init security/
-terraform apply -var 'db_snapshot_name='$db_snapshot_name'' -var-file="./security/terraform.tfvars" security/
+terraform apply -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress''  -var-file="./security/terraform.tfvars" security/
 
 echo #########################
 echo "please wait 5 mintues"
--- react/scripts/terraform/ec2_database/terraform.tfvars
+++ react/scripts/terraform/ec2_database/terraform.tfvars
@@ -1,19 +1,19 @@
 # access keys
-access_key = "AKIexample6435A"
-secret_key = "KsibdBexample1MPgOMPZ"
+access_key = "AKIAINPMAJNEHI7WKVCQ"
+secret_key = "NC8/1gN0T9PzeoejxxfMv662Od0UBz9u1PLrIf4H"
 
 # mysql information
 mysql_db_name = "db_example"
 mysql_user_name = "julia"
-mysql_password = "pexample45"
+mysql_password = "password12345"
 
-# user IP address
-user_ip_address = "79.196.191.136"
+## user IP address
+#user_ip_address = "127.0.0.1"
 
 # VPC information
 vpc_id = "vpc-d22288bb"
 security_group_id = "sg-79f66110"
 
 # PEM files
-key_name = "jim-gas"
-pem_file_location = "/path/to/jim-gas.pem"
+key_name = "jim-gastrofix"
+pem_file_location = "/home/jpizagno/AWS/jim-gastrofix.pem"
