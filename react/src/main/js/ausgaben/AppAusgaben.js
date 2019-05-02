363686eec2fd1c944e6b79a930c325e9a2d60e15
jpizagno <jpizagno@gmail.com>
2019-05-02 21:47:49 +0200

    refactored code

--- react/src/main/js/ausgaben/AppAusgaben.js
+++ react/src/main/js/ausgaben/AppAusgaben.js
@@ -1,5 +1,10 @@
 const React = require('react');
 
+import EditableCell from './EditableCell.js';
+import ProductRow from './ProductRow.js';
+import ProductTable from './ProductTable.js';
+import SearchBar from './SearchBar.js';
+
 class AppAusgaben extends React.Component {
 
     constructor(props) {
@@ -103,113 +108,5 @@ class AppAusgaben extends React.Component {
     }
   
   }
-  class SearchBar extends React.Component {
-    handleChange() {
-      this.props.onUserInput(this.refs.filterTextInput.value);
-    }
-    render() {
-      return (
-        <div>
   
-          <input type="text" placeholder="Search..." value={this.props.filterText} ref="filterTextInput" onChange={this.handleChange.bind(this)}/>
-  
-        </div>
-  
-      );
-    }
-  
-  }
-  
-  class ProductTable extends React.Component {
-  
-    render() {
-      var onProductTableUpdate = this.props.onProductTableUpdate;
-      var rowDel = this.props.onRowDel;
-      var filterText = this.props.filterText;
-      var product = this.props.products.map(function(product) {
-        if (product.name.indexOf(filterText) === -1) {
-          return;
-        }
-        return (<ProductRow onProductTableUpdate={onProductTableUpdate} product={product} onDelEvent={rowDel.bind(this)} key={product.id}/>)
-      });
-      return (
-        <div>
-  
-  
-        <button type="button" onClick={this.props.onRowAdd} className="btn btn-success pull-right">Add</button>
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>price</th>
-                <th>quantity</th>
-                <th>category</th>
-              </tr>
-            </thead>
-  
-            <tbody>
-              {product}
-  
-            </tbody>
-  
-          </table>
-        </div>
-      );
-  
-    }
-  
-  }
-  
-  class ProductRow extends React.Component {
-    onDelEvent() {
-      this.props.onDelEvent(this.props.product);
-  
-    }
-    render() {
-  
-      return (
-        <tr className="eachRow">
-          <EditableCell onProductTableUpdate={this.props.onProductTableUpdate} cellData={{
-            "type": "name",
-            value: this.props.product.name,
-            id: this.props.product.id
-          }}/>
-          <EditableCell onProductTableUpdate={this.props.onProductTableUpdate} cellData={{
-            type: "price",
-            value: this.props.product.price,
-            id: this.props.product.id
-          }}/>
-          <EditableCell onProductTableUpdate={this.props.onProductTableUpdate} cellData={{
-            type: "qty",
-            value: this.props.product.qty,
-            id: this.props.product.id
-          }}/>
-          <EditableCell onProductTableUpdate={this.props.onProductTableUpdate} cellData={{
-            type: "category",
-            value: this.props.product.category,
-            id: this.props.product.id
-          }}/>
-          <td className="del-cell">
-            <input type="button" onClick={this.onDelEvent.bind(this)} value="X" className="del-btn"/>
-          </td>
-        </tr>
-      );
-  
-    }
-  
-  }
-  class EditableCell extends React.Component {
-  
-    render() {
-      return (
-        <td>
-          <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onProductTableUpdate}/>
-        </td>
-      );
-  
-    }
-  
-  }
-
-
 export default AppAusgaben;
