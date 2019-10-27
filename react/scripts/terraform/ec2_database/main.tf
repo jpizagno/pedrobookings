b6244357d781dfc2aa92d03f2dabfbd1a2d4c7fc
jpizagno <jpizagno@gmail.com>
2019-10-27 09:19:29 +0100

    changes done automatically after upgrade

--- react/scripts/terraform/ec2_database/main.tf
+++ react/scripts/terraform/ec2_database/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  version = "~> 2.7"
-  access_key = "${var.access_key}"  # from variables.tf
-  secret_key = "${var.secret_key}" # from variables.tf
-  region     = "${var.region}"  # from variables.tf
+  version    = "~> 2.7"
+  access_key = var.access_key # from variables.tf
+  secret_key = var.secret_key # from variables.tf
+  region     = var.region     # from variables.tf
 }
 
 resource "aws_security_group" "bind_ec2_db_2" {
   name        = "bind_ec2_db_2"
   description = "allow 3306 to EC2 to DB insnace to User"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 8092
@@ -32,57 +32,59 @@ resource "aws_security_group" "bind_ec2_db_2" {
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
 
 # inclucde DB here as well, so Terraform does not delete it
 resource "aws_db_instance" "default" {
-  allocated_storage    = 20  #  GBytes, minimum is 20 GB
-  storage_type         = "gp2"  #  "gp2" (general purpose SSD)
-  engine               = "mysql"
-  engine_version       = "5.6.39"
-  instance_class       = "db.t2.micro"
-  name                 = "${var.mysql_db_name}"
-  username             = "${var.mysql_user_name}"
-  password             = "${var.mysql_password}"
-  port                 = "3306"
-  db_subnet_group_name = "default"
-  parameter_group_name = "default.mysql5.6"
-  publicly_accessible =  "true"
-  vpc_security_group_ids = ["${aws_security_group.bind_ec2_db_2.id}"]
-  skip_final_snapshot = true
-  snapshot_identifier = "${var.db_snapshot_name}"
+  allocated_storage      = 20    #  GBytes, minimum is 20 GB
+  storage_type           = "gp2" #  "gp2" (general purpose SSD)
+  engine                 = "mysql"
+  engine_version         = "5.6.39"
+  instance_class         = "db.t2.micro"
+  name                   = var.mysql_db_name
+  username               = var.mysql_user_name
+  password               = var.mysql_password
+  port                   = "3306"
+  db_subnet_group_name   = "default"
+  parameter_group_name   = "default.mysql5.6"
+  publicly_accessible    = "true"
+  vpc_security_group_ids = [aws_security_group.bind_ec2_db_2.id]
+  skip_final_snapshot    = true
+  snapshot_identifier    = var.db_snapshot_name
 }
 
 resource "aws_instance" "example_jim" {
-  ami = "ami-9a91b371"   
-  instance_type = "t2.micro"
-  key_name = "${var.key_name}" 
-  vpc_security_group_ids = ["${aws_security_group.bind_ec2_db_2.id}"]
+  ami                    = "ami-9a91b371"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.bind_ec2_db_2.id]
 
-   provisioner "file" {
+  provisioner "file" {
     connection {
-     user = "ec2-user"
-     host = "${aws_instance.example_jim.public_ip}"
-     agent = false
-     private_key = "${file("/home/jpizagno/AWS/jim-gastrofix.pem")}"
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = aws_instance.example_jim.public_ip
+      agent       = false
+      private_key = file("/home/jpizagno/AWS/jim-gastrofix.pem")
     }
     source      = "../setup_aws_docker.sh"
     destination = "/tmp/setup_aws_docker.sh"
   }
-  
-    # run script on machine
+
+  # run script on machine
+  # run script on machine
   provisioner "remote-exec" {
     connection {
-      user = "ec2-user"
-      host = "${aws_instance.example_jim.public_ip}"
-      agent = false
-      private_key = "${file("/home/jpizagno/AWS/jim-gastrofix.pem")}"
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = aws_instance.example_jim.public_ip
+      agent       = false
+      private_key = file("/home/jpizagno/AWS/jim-gastrofix.pem")
     }
     inline = [
       "chmod +x /tmp/setup_aws_docker.sh",
@@ -90,24 +92,25 @@ resource "aws_instance" "example_jim" {
       "sudo sed -i -e 's/julia/${var.mysql_user_name}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
       "sudo sed -i -e 's/james76/${var.mysql_password}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
       "sudo sed -i -e 's/localhost/${aws_db_instance.default.endpoint}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
-	    "cd ./bookingbootstrap/react/",
-	    "sudo ./docker_build.sh",
-	    "sudo ./docker_run.sh"
+      "cd ./bookingbootstrap/react/",
+      "sudo ./docker_build.sh",
+      "sudo ./docker_run.sh",
     ]
   }
 }
 
 # these have to be included in output, so that the next step can read them as "data.terraform_remote_state.folder_parent.aws_instance.example_jim.private_ip"
 output "aws_instance_example_jim_private_ip" {
-  value = "${aws_instance.example_jim.private_ip}"
+  value = aws_instance.example_jim.private_ip
 }
 
 # these have to be included in output, so that the next step can read them as "data.terraform_remote_state.folder_parent.aws_security_group.bind_ec2_db_2.id"
 output "aws_security_group_bind_ec2_db_2_id" {
-  value = "${aws_security_group.bind_ec2_db_2.id}"
+  value = aws_security_group.bind_ec2_db_2.id
 }
 
 # export endpoint for fixing
 output "aws_db_instance_default_endpoint" {
-  value = "${aws_db_instance.default.endpoint}"
+  value = aws_db_instance.default.endpoint
 }
+
--- react/scripts/terraform/ec2_database/variables.tf
+++ react/scripts/terraform/ec2_database/variables.tf
@@ -1,13 +1,32 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "key_name" {}
-variable "mysql_db_name" {}
-variable "mysql_user_name" {}
-variable "mysql_password" {}
-variable "user_ip_address" {}
-variable "vpc_id" {}
-variable "pem_file_location" {}
-variable "db_snapshot_name" {}
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
+
+variable "key_name" {
+}
+
+variable "mysql_db_name" {
+}
+
+variable "mysql_user_name" {
+}
+
+variable "mysql_password" {
+}
+
+variable "user_ip_address" {
+}
+
+variable "vpc_id" {
+}
+
+variable "pem_file_location" {
+}
+
+variable "db_snapshot_name" {
+}
 
 # one example of variable
 variable "region" {
@@ -15,8 +34,9 @@ variable "region" {
 }
 
 variable "amis" {
-  type = "map"
+  type = map(string)
   default = {
     "eu-central-1" = "ami-3a70df55"
   }
 }
+
