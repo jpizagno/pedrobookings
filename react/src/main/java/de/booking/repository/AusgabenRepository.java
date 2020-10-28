92482b1b25654c82162edcc666d530d114be70d7
jpizagno <jpizagno@gmail.com>
2020-10-28 21:54:09 +0100

    removed AusgabenRepo

--- react/src/main/java/de/booking/repository/AusgabenRepository.java
+++
@@ -1,6 +0,0 @@
-package de.booking.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AusgabenRepository extends PagingAndSortingRepository<Ausgaben, Long> {
-}
