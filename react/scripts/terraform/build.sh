c22908e2d566dc2be954d8db8923fc3fa89f968b
jpizagno <jpizagno@gmail.com>
2022-05-02 17:30:41 +0200

    removed security module for aws-cli option to add repo

--- react/scripts/terraform/build.sh
+++ react/scripts/terraform/build.sh
@@ -16,6 +16,10 @@ echo
 read -p 'Enter AWS AWSSecretKey (see ~/.aws/credentials) : ' secret_key
 echo
 
+current_working_directory=$(pwd)
+echo "using current_working_directory of "$current_working_directory
+echo
+
 export TF_LOG=INFO
 export TF_LOG_PATH=./terraform.log
 
@@ -25,16 +29,19 @@ export TF_LOG_PATH=./terraform.log
 
 # terraform v1.1.6
 terraform -chdir=./ec2_database/ init
-terraform -chdir=./ec2_database/ apply -auto-approve -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress'' -var 'access_key='$access_key'' -var 'secret_key='$secret_key''  -var-file="terraform.tfvars"
+terraform -chdir=./ec2_database/ apply -auto-approve -var 'current_working_directory='$current_working_directory'' -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress'' -var 'access_key='$access_key'' -var 'secret_key='$secret_key''  -var-file="terraform.tfvars"
 
 ## Terraform v0.12.9
 #terraform init security/
 #terraform apply -auto-approve -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress'' -var 'access_key='$access_key'' -var 'secret_key='$secret_key''  -var-file="./security/terraform.tfvars" security/
 
-# terraform v1.1.6
-terraform -chdir=./security/ init
-terraform -chdir=./security/ apply -auto-approve -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress'' -var 'access_key='$access_key'' -var 'secret_key='$secret_key''
+## terraform v1.1.6
+#terraform -chdir=./security/ init
+#terraform -chdir=./security/ apply -auto-approve -var 'db_snapshot_name='$db_snapshot_name'' -var 'user_ip_address='$ipaddress'' -var 'access_key='$access_key'' -var 'secret_key='$secret_key''
 
+securit_group_id=$(grep '"id":' ec2_database/terraform.tfstate  | grep 'sg-' | awk '{print $2}' | tr -d '"' | tr -d ',')
+ec2_private_ip=$(grep '"private_ip":' ec2_database/terraform.tfstate | awk '{print $2}' | tr -d '"' | tr -d ',')
+aws ec2 authorize-security-group-ingress --group-id $securit_group_id --protocol tcp --port 3306 --cidr $ec2_private_ip/32
 
 echo #########################
 echo "please wait 5 mintues"
--- react/scripts/terraform/ec2_database/main.tf
+++ react/scripts/terraform/ec2_database/main.tf
@@ -72,7 +72,7 @@ resource "aws_instance" "example_jim" {
       agent       = false
       private_key = file("/home/jpizagno/AWS/jim-gastrofix.pem")
     }
-    source      = "../setup_aws_docker.sh"
+    source      = "${var.current_working_directory}/../setup_aws_docker.sh"
     destination = "/tmp/setup_aws_docker.sh"
   }
 
@@ -89,7 +89,7 @@ resource "aws_instance" "example_jim" {
     inline = [
       "chmod +x /tmp/setup_aws_docker.sh",
       "/tmp/setup_aws_docker.sh",
-      "sudo sed -i -e 's/julia/${var.mysql_user_name}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
+      "sudo sed -i -e 's/root/${var.mysql_user_name}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
       "sudo sed -i -e 's/james76/${var.mysql_password}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
       "sudo sed -i -e 's/localhost/${aws_db_instance.default.endpoint}/g' ./bookingbootstrap/react/src/main/resources/application.properties",
       "cd ./bookingbootstrap/react/",
