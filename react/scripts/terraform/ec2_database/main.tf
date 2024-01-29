65c60fb8d05b2b44f315de7db62aaa92f8120b50
jpizagno <jpizagno@gmail.com>
2024-01-29 14:16:51 +0100

    upgraded mysql from 8.0.28 to 8.0.33

--- react/scripts/terraform/ec2_database/main.tf
+++ react/scripts/terraform/ec2_database/main.tf
@@ -44,7 +44,7 @@ resource "aws_db_instance" "default" {
   allocated_storage      = 20    #  GBytes, minimum is 20 GB
   storage_type           = "gp2" #  "gp2" (general purpose SSD)
   engine                 = "mysql"
-  engine_version         = "8.0.28"
+  engine_version         = "8.0.33"
   instance_class         = "db.t2.micro"
   name                   = var.mysql_db_name
   username               = var.mysql_user_name
